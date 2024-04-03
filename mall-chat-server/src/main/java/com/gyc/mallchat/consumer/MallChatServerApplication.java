61c8f9507dcb2e23037c5d78b848a67a1d3cec69
gyc <3119958142@qq.com>
2024-04-03 10:48:10 +0800

    第一次学习

---
+++ mall-chat-server/src/main/java/com/gyc/mallchat/consumer/MallChatServerApplication.java
@@ -0,0 +1,20 @@
+package com.gyc.mallchat.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: MallChatServerApplication
+ * Package: com.gyc.mallchat
+ * Description:
+ *
+ * @Author gyc
+ * @Create 2024/4/2 17:01
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MallChatServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallChatServerApplication.class, args);
+    }
+}
---
+++ mall-chat-server/src/main/java/com/gyc/mallchat/consumer/websocket/NettyWebSocketServer.java
@@ -0,0 +1,9 @@
+package com.gyc.mallchat.consumer.websocket;/**
+* ClassName: NettyWebSocketServer
+* Package: com.gyc.mallchat.consumer.websocket
+* Description: 
+* @Author gyc
+* @Create 2024/4/2 17:20 
+* @Version 1.0   
+*/public class NettyWebSocketServer {
+}
